345a1388a84ca50145a134e49cf3dc94854feba8
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2022-04-28 23:03:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ canvas = document.getElementById("canvas")
 ctx = canvas.getContext("2d")
 
 const friction = 2;
-const levelnum = 10;
+const levelnum = 11;
 const holerad = 10;
 let level = 0;
 let ballvel = [0, 0];
@@ -15,13 +15,15 @@ let death = [];
 
 let getmousedown;
 
+let win = false;
+
 let triangleA = 0
 let triangleB = 0
 
 
 let trigstart = [0, 0]
 
-let levels = {
+let levels = [
     /* 
     ball start  
     hole pos
@@ -29,84 +31,96 @@ let levels = {
     sands
     deaths
     */
-    l1: [
+    [
         [10, 10],
         [300, 300],
         [],
         [],
         []
     ],
-    l2: [
+    [
         [10, 15],
         [300, 300],
-        [],
+        [
+            [50, 50, 100, 100],
+            [500, 200, 100, 100]
+        ],
         [],
         []
     ],
-    l3: [
+    [
         [10, 20],
         [300, 300],
         [],
         [],
         []
     ],
-    l4: [
+    [
         [10, 25],
         [300, 300],
         [],
         [],
         []
     ],
-    l5: [
+    [
         [10, 30],
         [300, 300],
         [],
         [],
         []
     ],
-    l6: [
+    [
         [15, 10],
         [300, 300],
         [],
         [],
         []
     ],
-    l7: [
+    [
         [20, 10],
         [300, 300],
         [],
         [],
         []
     ],
-    l8: [
+    [
         [25, 10],
         [300, 300],
         [],
         [],
         []
     ],
-    l9: [
+    [
         [30, 10],
         [300, 300],
         [],
         [],
         []
     ],
-    l10: [
+    [
         [35, 35],
         [300, 300],
         [],
         [],
         []
+    ],
+    [
+        [105, 105],
+        [300, 300],
+        [
+            [120, 120, 150, 150]
+        ],
+        [],
+        []
     ]
-}
+]
 
 for (i = 0; i < levelnum; i++) {
-    ball[i] = levels.l1[0]
-    hole[i] = new Array(2)
-    walls[i] = new Array(4)
-    sand[i] = new Array(4)
-    death[i] = new Array(4)
+    ball[i] = levels[i][0]
+    hole[i] = levels[i][1]
+    walls[i] = levels[i][2]
+    sand[i] = levels[i][3]
+    death[i] = levels[i][4]
 }
 
 window.addEventListener("mousedown", startTrig)
@@ -119,7 +133,6 @@ function draw() {
 }
 
 function fillCircle(pos, rad) {
-    ctx.fillStyle = "blue"
     ctx.beginPath()
     ctx.arc(pos[0], pos[1], rad, 0, 2 * Math.PI)
         //ctx.stroke()
@@ -139,7 +152,12 @@ function startTrig() {
 
 function releaseBall() {
     getmousedown = 0
-    ballvel = [triangleA / 10, triangleB / 10]
+    if (!getinsand()) {
+        ballvel = [triangleA / 10, triangleB / 10]
+    } else {
+        ballvel = [triangleA / 30, triangleB / 30]
+    }
+
     window.removeEventListener("mousemove", drawline)
     window.addEventListener("mousedown", startTrig)
     window.removeEventListener("mouseup", releaseBall)
@@ -168,17 +186,93 @@ load()
 
 function redraw() {
     ctx.clearRect(0, 0, 1000, 700)
+    ctx.fillStyle = "black"
+    fillCircle(hole[level], holerad)
+        //walls
+    for (i = 0; i < walls[level].length; i++) {
+        ctx.fillStyle = "#A0FFF0"
+        ctx.fillRect(walls[level][i][0], walls[level][i][1], walls[level][i][2], walls[level][i][3])
+    }
+    //sand
+    for (i = 0; i < sand[level].length; i++) {
+        ctx.fillRect(sand[level][i][0], sand[level][i][1], sand[level][i][2], sand[level][i][3])
+    }
+    //death
+    for (i = 0; i < death[level].length; i++) {
+        ctx.fillRect(death[level][i][0], death[level][i][1], death[level][i][2], death[level][i][3])
+    }
+    ctx.fillStyle = "blue"
     fillCircle(ballcurr, holerad)
+
     if (getmousedown) {
         ctx.beginPath()
         ctx.moveTo(trigstart[0] + triangleA, trigstart[1] + triangleB)
         ctx.lineTo(trigstart[0], trigstart[1])
         ctx.stroke()
     }
-    //drawline()
+    if (win) {
+        ctx.font = "serif"
+        ctx.strokeText("You Win", 450, 200, 100)
+    }
 }
 
 movereps = 0;
+
+function getinsand() {
+
+}
+
+let vertex = [0, 0]
+
+function wallbounds() {
+    fix = 0
+
+    //wall bounds
+    for (i = 0; i < walls[level].length; i++) {
+        Wx = walls[level][i][0]
+        Wy = walls[level][i][1]
+        Ww = walls[level][i][2]
+        Wh = walls[level][i][3]
+
+        vertices = [];
+        //calculate closest vertex
+        for (j = 0; j < 4; j++) {
+            switch (j) {
+                case 0:
+                    vertex = [Wx, Wy]
+                    break;
+                    //console.log(vertex)
+                case 1:
+                    vertex = [Wx + Ww, Wy]
+                    break;
+                    //console.log(vertex)
+                case 2:
+                    vertex = [Wx, Wy + Wh]
+                    break;
+                    //console.log(vertex)
+                case 3:
+                    vertex = [Wx + Ww, Wy + Wh]
+                    break;
+                    //console.log(vertex)
+
+            }
+            vertices.push(Math.pow(Math.pow(vertex[0] - xmove, 2) + Math.pow(vertex[1] - ymove, 2), 0.5))
+        }
+
+        smol = vertices.findIndex(function(a) { return a == Math.min([vertices[0], vertices[1], vertices[2], vertices[3]]) })
+
+
+        if (xmove + holerad >= Wx && ymove + holerad >= Wy && xmove - holerad < Wx + Ww && ymove - holerad <= Wy + Wh) {
+
+            fix = 1
+            console.log("hiii")
+        }
+        //if (xmove >= Wx && ymove + holerad >= Wy && xmove - holerad < Wx + Ww && ymove - holerad <= Wy + Wh && !fix) {
+        //    yvel *= -1
+        //    console.log("hiii")
+        //}
+    }
+}
 
 function moveball() {
 
@@ -188,31 +282,45 @@ function moveball() {
     yvel = ballvel[1]
     x = ballcurr[0]
     y = ballcurr[1]
+    xmove = x + xvel
+    ymove = y + yvel
 
     if (movereps < 10000 && ballvel != [0, 0]) {
         redraw()
 
-
-        if (x + xvel <= 0 || x + xvel >= 1000) {
+        //canvas bounds
+        if (xmove <= 0 || xmove >= 1000) {
             xvel *= -1
         }
-        if (y + yvel <= 0 || y + yvel >= 700) {
+        if (ymove <= 0 || ymove >= 700) {
             yvel *= -1
         }
 
+        wallbounds()
+
+        //take the x position and y position then find the x dist and y dist from the hole pos then use pythag to find direct distance
+
+        pythag1 = hole[level][0] - xmove
+        pythag2 = hole[level][1] - ymove
+
+        if (Math.pow((Math.pow(pythag1, 2) + Math.pow(pythag2, 2)), 0.5) - holerad / 2 <= holerad) { // square roots the addition of a^2 + b^2
+
+            nextlvl()
+
+        }
         x += xvel
         y += yvel
 
-        xvel *= 0.95
-        yvel *= 0.95
+        xvel *= 0.92
+        yvel *= 0.92
 
-        if (Math.abs(xvel) <= 0.5) {
-            xvel = 0
-        }
-
-        if (Math.abs(yvel) <= 0.5) {
-            yvel = 0
-        }
+        //if (Math.abs(xvel) <= 0.5) {
+        //    xvel = 0
+        //}
+        //
+        //if (Math.abs(yvel) <= 0.5) {
+        //    yvel = 0
+        //}
 
 
         ballvel = [xvel, yvel]
@@ -221,5 +329,22 @@ function moveball() {
         movereps++
         return
     }
+}
+
+function nextlvl() {
+
+    if (level < 9) {
+
+        xvel = 0
+        yvel = 0
+        level++
+        x = 0
+        y = 0
+    } else {
+        level = 10
+        win = true
+    }
+
+
 }
 setInterval(moveball, 1000 * (1 / 60))
